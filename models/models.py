1d5fd877413f7f6cc30fa89777ee0364c8980ef5
ismailMd378 <ismailmechbal@inspladelestany.cat>
2024-04-15 15:52:17 +0200

    Crear Taula Reserva

--- models/models.py
+++ models/models.py
@@ -7,5 +7,11 @@ class RutaBicicleta(models.Model):
     CodiReserva = fields.Char('Codi Reserva' , size=7, required=True)
     DataArrivada = fields.Date('Date Arivada')
     NitExtra = fields.Boolean('Nit Extra')
+    Num_Bicis = fields.Integer('Num De Bicis')
+    DataReserva = fields.Date('Data Reserva')
+    CodiIntermidiari = fields.Many2one('Intermidiari.model', string = 'Codi_Intermidiari')
+    CodiGrup = fields.Many2one('Codi ')
+    GrupRuta = fields.Many2one('Grup.model', string = 'CodiGrup')
+
     
 
